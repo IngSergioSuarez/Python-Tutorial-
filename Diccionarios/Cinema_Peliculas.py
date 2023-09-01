f894e5ada064a8bc9c017595e7be3bebbb1a1a2b
Sergio Suarez Monje <checho.soluciones@gmail.com>
2023-09-01 16:15:53 -0500

    Ejercicios Diccionarios

---
+++ Diccionarios/Cinema_Peliculas.py
@@ -0,0 +1,7 @@
+"""
+hacer un sistema de reserva de tiquetes de un cinema. edad minima 
+"""
+
+cartelera = {
+    "Bolt" : {"Unicentro" : 10, "Cedritos" : 10, "Colina" : 10, "Santa fe" : 10}
+}
---
+++ Diccionarios/Estudiantes2.py
@@ -0,0 +1,19 @@
+"""
+La idea del codigo es hacer un diccionario que contenga informacion de numero de identificacion, Profesion y edad 
+"""
+
+estudiantes = {}
+
+#Podemos ingresar Listas con difenrentes valores en una 
+
+estudiantes = {
+    "Juliama": ["ID0001", "Medicina", 29],
+    "Oscar" : ["ID0002", "Comunicación", 29],
+    "Julian" : ["ID0003", "Ing. Ambiental", 24],
+    "Paula" : ["ID0004", "Arquitecta", 28],
+    "Felipe" : ["ID0005", "Abogado", 32],
+    "Sergio" : ["ID0006", "Ing. Mecánico", 29]
+}
+
+print(estudiantes)
+
