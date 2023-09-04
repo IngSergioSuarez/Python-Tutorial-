d48a6b1d88195af0d968795a41ef48cbf070ff41
IngSergioSuarez <checho.soluciones@gmail.com>
2023-09-04 12:50:14 -0500

    no terminado

--- Diccionarios/Cinema_Peliculas.py
+++ Diccionarios/Cinema_Peliculas.py
@@ -3,5 +3,9 @@ hacer un sistema de reserva de tiquetes de un cinema. edad minima
 """
 
 cartelera = {
-    "Bolt" : {"Unicentro" : 10, "Cedritos" : 10, "Colina" : 10, "Santa fe" : 10}
+    "Bolt" : {"Unicentro" : 10, "Cedritos" : 10, "Colina" : 10, "Santa fe" : 10},
+    "Jon Wick" : {"Unicentro" : 10, "Cedritos" : 10, "Colina" : 10, "Santa fe" : 10},
+    "Constantine" : {"Unicentro" : 10, "Cedritos" : 10, "Colina" : 10, "Santa fe" : 10}
 }
+
+print("¡Bienvenido a su compañia de cine!, por favor, escoja una opcion de nuestro Menu: ")
