75e63ba7dbf05b6a110c3ab5209f096c381d0891
IngSergioSuarez <checho.soluciones@gmail.com>
2023-12-24 22:39:03 -0500

    Create the Website for the notes

---
+++ cases_blog/myBlog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DeleteView
+from .models import Post
+
+# Create your views here.
+
+#def home(request):
+#    return render(request, 'home.html', {})
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class ArticleDetailView(DeleteView):
+    model = Post
+    template_name = 'article_details.html'
